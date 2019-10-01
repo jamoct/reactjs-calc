4a6f117ef18337e6cd9e8397df90a7606a5fb3d6
joyce4 <joyce@barcelonacodeschool.com>
2019-10-02 00:07:42 +0200

    Updates

---
+++ precache-manifest.07d8d3a106f871577266e08e3e6ff234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e465b92180e698a5967481e804983a",
+    "url": "/reactjs-calc/index.html"
+  },
+  {
+    "revision": "4a868be666a15649ab57",
+    "url": "/reactjs-calc/static/css/main.df1adddd.chunk.css"
+  },
+  {
+    "revision": "8a7afe5a30a02c573497",
+    "url": "/reactjs-calc/static/js/2.5676f820.chunk.js"
+  },
+  {
+    "revision": "4a868be666a15649ab57",
+    "url": "/reactjs-calc/static/js/main.b604bed1.chunk.js"
+  },
+  {
+    "revision": "735b792212909d7a3143",
+    "url": "/reactjs-calc/static/js/runtime-main.d38845b0.js"
+  }
+]);
